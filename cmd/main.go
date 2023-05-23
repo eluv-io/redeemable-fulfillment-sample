dbb7faa70530b1771e09b67d4c09aa8b360ad2bc
Todd Hodes <todd.hodes@eluv.io>
2023-05-22 22:43:43 -0700

    clean up main

--- cmd/main.go
+++ cmd/main.go
@@ -20,77 +20,114 @@ type ConfigState struct {
 	Verbosity  string
 }
 
-func toVerbosity(verbosity int) string {
-	switch verbosity {
-	case 0:
-		return "fatal"
-	case 1:
-		return "error"
-	case 2:
-		return "warn"
-	case 3:
-		return "info"
-	case 4:
-		return "debug"
-	case 5:
-		return "trace"
-	default:
-		panic("bad verbosity level")
+const (
+	DaemonName = "fulfillmentd"
+)
+
+var (
+	cfgState = ConfigState{}
+	log      = elog.Get("/fs")
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Sprintf("Usage: %s --config <config.toml>\n", DaemonName)
+		return
+	}
+
+	if _, err := startServer(os.Args[2]); err != nil {
+		fmt.Println("cannot launch", "Error", err)
+		os.Exit(1)
 	}
 }
 
-func HandleConfig(cfg *ConfigState, prefix string) error {
+func startServer(configFile string) (s *authority.Server, err error) {
+	cfg, err := loadConfig(configFile)
+	if err != nil {
+		return
+	}
+
+	s, err = authority.ConnectDb(cfg)
+	if err != nil {
+		return
+	}
+
+	err = fulfillmentd.Init(s)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func loadConfig(configFile string) (cfg *config.AuthorityConfig, err error) {
+	log.Debug("config", "file", configFile)
+	viper.SetDefault(DaemonName+".service_port", 2023)
+	viper.SetDefault(DaemonName+".log_file", DaemonName)
+	viper.SetDefault(DaemonName+".log_handler", "console")
+	viper.SetDefault(DaemonName+".verbosity", 3)
+	viper.SetConfigFile(configFile)
+
+	cfg = &config.AuthorityConfig{}
+	err = getBaseConfig(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	var trueVal = true
+	logConfig := &elog.Config{
+		Level:   cfgState.Verbosity,
+		Handler: cfgState.LogHandler,
+		File:    &elog.LumberjackConfig{Filename: cfgState.LogFile},
+		Caller:  &trueVal,
+	}
+	elog.SetDefault(logConfig)
+
+	if lh, ok := log.Handler().(*console.Handler); ok {
+		lh.WithTimestamps(true)
+	}
+
+	log.Debug("loadConfig", "service_port", cfg.Port)
+
+	return cfg, nil
+}
+
+func getBaseConfig(cfg *config.AuthorityConfig) (err error) {
+	if err = loadConfigState(&cfgState, DaemonName); err != nil {
+		log.Error("error parsing", "config file", err)
+		return
+	}
+
+	if cfg.DbConfig, err = getDbConfig(); err != nil {
+		log.Error("getDbConfig error", err)
+		return
+	}
+
+	cfg.Port = viper.GetInt(DaemonName + ".service_port")
+
+	return
+}
+
+func loadConfigState(configState *ConfigState, prefix string) error {
 	var filename string
 	var err error
 
-	filename = filepath.Base(cfg.Config)
+	filename = filepath.Base(configState.Config)
 	viper.SetConfigName(filename[:len(filename)-len(filepath.Ext(filename))])
-	viper.AddConfigPath(filepath.Dir(cfg.Config))
+	viper.AddConfigPath(filepath.Dir(configState.Config))
 
 	if err = viper.ReadInConfig(); err != nil {
 		return fmt.Errorf("failed to read config file - %v", err)
 	}
 
-	cfg.LogFile = viper.GetString(prefix + ".log_file")
-	cfg.LogHandler = viper.GetString(prefix + ".log_handler")
-	cfg.Verbosity = toVerbosity(viper.GetInt(prefix + ".verbosity"))
+	configState.LogFile = viper.GetString(prefix + ".log_file")
+	configState.LogHandler = viper.GetString(prefix + ".log_handler")
+	configState.Verbosity = toVerbosity(viper.GetInt(prefix + ".verbosity"))
 
 	return nil
 }
 
-var (
-	trueVal = true
-
-	cfgState = ConfigState{}
-
-	readConfig = func() error {
-		if err := HandleConfig(&cfgState, "fulfillmentd"); err != nil {
-			log.Error("error parsing", "config file", err)
-			return err
-		}
-		return nil
-	}
-
-	log = elog.Get("/fs/fulfillmentd")
-)
-
-func getBaseConfig(cfg *config.AuthorityConfig) (err error) {
-	if err = readConfig(); err != nil {
-		log.Error("readConfig error", err)
-		return
-	}
-
-	if cfg.DbConfig, err = getDbConfig(cfg); err != nil {
-		log.Error("getDbConfig error", err)
-		return
-	}
-
-	cfg.Port = viper.GetInt("fulfillmentd.service_port")
-
-	return
-}
-
-func getDbConfig(_ *config.AuthorityConfig) (dbCfg config.DbConfig, err error) {
+func getDbConfig() (dbCfg config.DbConfig, err error) {
 	dbCfg = config.DbConfig{
 		Username:      viper.GetString("db.username"),
 		Password:      viper.GetString("db.password"),
@@ -118,67 +155,21 @@ func getDbConfig(_ *config.AuthorityConfig) (dbCfg config.DbConfig, err error) {
 	return
 }
 
-func Config(configFile string) (cfg *config.AuthorityConfig, err error) {
-	log.Debug("config", "file", configFile)
-	viper.SetDefault("fulfillmentd.service_port", 2023)
-	viper.SetDefault("fulfillmentd.log_file", "fulfillmentd")
-	viper.SetDefault("fulfillmentd.log_handler", "console")
-	viper.SetDefault("fulfillmentd.verbosity", 3)
-	viper.SetConfigFile(configFile)
-
-	cfg = &config.AuthorityConfig{}
-	err = getBaseConfig(cfg)
-	if err != nil {
-		return nil, err
-	}
-
-	logConfig := &elog.Config{
-		Level:   cfgState.Verbosity,
-		Handler: cfgState.LogHandler,
-		File:    &elog.LumberjackConfig{Filename: cfgState.LogFile},
-		Caller:  &trueVal,
-	}
-	elog.SetDefault(logConfig)
-
-	if lh, ok := log.Handler().(*console.Handler); ok {
-		lh.WithTimestamps(true)
-	}
-
-	log.Debug("ports", "service_port", cfg.Port)
-	log.Debug("BaseConfig", "cfg", cfg)
-
-	return cfg, nil
-}
-
-func StartServer(configFile string) (s *authority.Server, err error) {
-	cfg, e := Config(configFile)
-	if e != nil {
-		return nil, err
-	}
-
-	s, err = authority.ConnectDb(cfg)
-	if err != nil {
-		return nil, err
-	}
-	log.Debug("StartServer", "Server:", s)
-
-	err = fulfillmentd.Init(s)
-	if err != nil {
-		return nil, err
-	}
-
-	return
-}
-
-func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: fulfillmentd --config <config.toml>")
-		return
-	}
-
-	if _, err := StartServer(os.Args[2]); err != nil {
-		fmt.Println("cannot launch", "Error", err)
-		log.Error("cannot launch", "Error", err)
-		os.Exit(1)
+func toVerbosity(verbosity int) string {
+	switch verbosity {
+	case 0:
+		return "fatal"
+	case 1:
+		return "error"
+	case 2:
+		return "warn"
+	case 3:
+		return "info"
+	case 4:
+		return "debug"
+	case 5:
+		return "trace"
+	default:
+		panic("bad verbosity level")
 	}
 }
