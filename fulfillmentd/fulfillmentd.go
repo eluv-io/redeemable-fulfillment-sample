ce6dfea7871b14ad7dbfd0ff3d59789eaa376c51
Todd Hodes <todd.hodes@eluv.io>
2023-05-30 13:22:22 -0700

    add default CORS

--- fulfillmentd/fulfillmentd.go
+++ fulfillmentd/fulfillmentd.go
@@ -16,6 +16,7 @@ var log = elog.Get("/fs")
 
 func Init(s *server.Server) error {
 	s.Router = gin.Default()
+	s.Router.Use(defaultCORS)
 
 	s.FulfillmentService = server.NewFulfillmentService(s)
 	log.Info("Init", "service", s.FulfillmentService)
@@ -54,4 +55,17 @@ func Version(ctx *gin.Context) {
 
 func GET(path string, handler gin.HandlerFunc) *server.Route {
 	return server.NewRoute("GET", path, handler)
+}
+
+func defaultCORS(ctx *gin.Context) {
+	ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+	ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+	if ctx.Request.Method == "OPTIONS" {
+		ctx.AbortWithStatus(http.StatusNoContent)
+		return
+	}
+	ctx.Next()
 }
